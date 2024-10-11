d77f9120e8a77e77a33225821674f82ffd897283
Moamed10 <144000841+Moamed10@users.noreply.github.com>
2024-10-11 15:55:04 +0200

    a

---
+++ advanced_js/ECMAScript_6/variables.js
@@ -0,0 +1,5 @@
+let num = 13
+if(num === 13){
+  let num = ( 55) 
+}
+console.log( num) //output: global value is 
--- advanced_js/first_CP/variables.js
+++
@@ -1,5 +0,0 @@
-let num = 13
-if(num === 13){
-  let num = console.log('local value is', num) //output: local value is 1
-}
-console.log('global value is', num) //output: global value is 
